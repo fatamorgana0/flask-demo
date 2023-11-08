1b5ffa06a1d797b371d8dac499437a9f71a15554
fatamorgana0 <148909283+fatamorgana0@users.noreply.github.com>
2023-11-08 01:38:51 +0000

    umm this shit is boring..

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+WARRIORS = [
+  {
+    'id' : 1,
+    'name' : 'Samukai',
+    'background' : 'Spinjitzu',
+    'avg pay for the job' : 'million credits'
+    
+  },
+  {
+    'id' : 2,
+    'name' : 'Belmont',
+    'background' : 'Romania',
+    'avg pay for the job' : '100 German franks'
+  },
+  {
+    'id' : 3,
+    'name' : 'Chupacabra',
+    'avg pay for the job' : '10000 $$'
+
+  }
+]
+@app.route("/warriors")
+def list_warriors():
+  return jsonify(WARRIORS)
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', warriors = WARRIORS, protagonist = "Jack")
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug = True)
